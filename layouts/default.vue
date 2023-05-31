21eae30e01cc71a6c70d2f07a6ca51add5f25fe9
AdrianoTyova <tyovaadriano@gmail.com>
2023-05-31 09:20:10 +0100

    menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,11 +7,45 @@
         <div class="PContainer">
             <NuxtPage />
         </div>
+        <div class="menubaixo pa-5 ">
+            <ul class="bg-green  pa-5 d-flex justify-space-between align-center">
+                <li>
+                    <nuxt-link to="/">Inicio</nuxt-link>
+                </li>
+                <li>
+                    <nuxt-link to="/plantacao/selecao">Processos</nuxt-link>
+                </li>
+                <li>
+                    <nuxt-link to="/vendas/provincias">Venda</nuxt-link>
+                </li>
+            </ul>
+        </div>
     </div>
 </div>
 </template>
 
-<style>
+<style lang="scss">
+.menubaixo {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+
+    ul {
+        margin: auto;
+        width: 100%;
+        list-style: none;
+        border-radius: 18px;
+        box-shadow: 0 0 5px #ccc;
+
+        li {
+            a {
+                color: #fff;
+            }
+        }
+    }
+}
+
 .principal {
     display: grid;
     grid-template-columns: .3fr .7fr;
@@ -19,8 +53,6 @@
     height: 100vh;
     overflow: hidden;
 }
-
-
 
 .PSide {
     width: 350px;
